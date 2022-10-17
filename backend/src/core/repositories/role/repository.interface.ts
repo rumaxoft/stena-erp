bcf7ef6e524155da9f65d6506c1b4f3043414e71
Maxim Rudenko <maximu.rudenko@gmail.com>
2022-10-17 16:55:29 +0500

    backend role repository interface fix findByTitle

--- backend/src/core/repositories/role/repository.interface.ts
+++ backend/src/core/repositories/role/repository.interface.ts
@@ -4,6 +4,6 @@ export interface RoleRepository {
   insert(role: RoleModel): Promise<RoleModel>;
   findById(id: UniqueId): Promise<RoleModel | null>;
   findAll(): Promise<RoleModel[]>;
-  findByTitle(title: string): Promise<RoleModel>;
+  findByTitle(title: string): Promise<RoleModel | null>;
   deleteById(id: UniqueId): Promise<void>;
 }
